7ce773f1188f201395af48374bc45f57266fa041
ChristAren <76263250+ChristAren@users.noreply.github.com>
2021-01-12 20:15:02 -0500

    ITS ALIVE

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,36 +1,35 @@
-
-
-
-function FizzBuzz(value1, value2){
-    let returnArray= [];
-    returnArray[0]="";
-    for (let loop =1; loop <= 100; loop++){
-        returnArray[loop] = ((loop%value1==0 ? 'Fizz' : '') + (loop%value2==0 ? 'Buzz' : '') || loop);
-    }   
-    return returnArray; 
+function FizzBuzz(value1, value2) {
+  let returnArray = [];
+  returnArray[0] = "";
+  for (let loop = 1; loop <= 100; loop++) {
+    returnArray[loop] =
+      (loop % value1 == 0 ? "Fizz" : "") + (loop % value2 == 0 ? "Buzz" : "") ||
+      loop;
+  }
+  return returnArray;
 }
 
+function buzzTable() {
+  let output = [];
+  let headTemplate = document.getElementById("fizzheader");
+  let rowTemplate = document.getElementById("fizzbuzz-row-items");
 
+  let templateHTML = rowTemplate.innerHTML;
+  let resultsHTML = headTemplate.innerHTML;
 
-function buzzTable(){
-    let output = [];
-    let headTemplate = document.getElementById("fizzheader");
-    let rowTemplate = document.getElementById("fizzbuzz-row-items");
+  let val1 = parseInt(document.getElementById("FizzValue").value);
+  let val2 = parseInt(document.getElementById("BuzzValue").value);
 
-    let templateHTML = headTemplate.innerHTML;
-    let resultsHTML = rowTemplate.innerHTML;
-
-    let val1 = parseInt(document.getElementById("FizzValue").value);
-    let val2 = parseInt(document.getElementById("BuzzValue").value);
-
-    output = FizzBuzz(val1,val2);
-    for(loop=1; loop<output.length; loop +=5){
-        resultsHTML += templateHTML.replace('{{val1}}', output[loop])
-                                        .replace('{{val2}}', output[loop+1])
-                                        .replace('{{val3}}', output[loop+2])
-                                        .replace('{{val4}}', output[loop+3])
-                                        .replace('{{val5}}', output[loop+4]);
-    }
+  output = FizzBuzz(val1, val2);
+  for (loop = 1; loop < output.length; loop += 5) {
+    resultsHTML += templateHTML
+      .replace("{{val1}}", output[loop])
+      .replace("{{val2}}", output[loop + 1])
+      .replace("{{val3}}", output[loop + 2])
+      .replace("{{val4}}", output[loop + 3])
+      .replace("{{val5}}", output[loop + 4]);
+  }
+  document.getElementById("results").innerHTML = resultsHTML;
 }
 
 // The premise of FizzBuzz is to take 2 user inputs and find all of their multiples between 1 and 100
